b4ca769ab48c9a6364496547bb1f3fb318715543
Tina Huynh <tinamchuynh@gmail.com>
2023-02-19 10:33:19 -0800

    documentation

--- public/js/app.js
+++ public/js/app.js
@@ -7,6 +7,10 @@ $(document).ready(function () {
     var countLama;
     var posLama;
     var count;
+    /* Creating a clock object that is a countdown clock that starts at 0 and has a callback function
+    that checks if the clock is at 0. If it is, it checks if the position is session or break. If it
+    is session, it sets the clock to the break time and starts the clock. If it is break, it sets
+    the clock to the session time and starts the clock. */
     var clock = $(".timer").FlipClock(0, {
         countdown: true,
         clockFace: 'MinuteCounter',
@@ -28,6 +32,9 @@ $(document).ready(function () {
         }
     })
     //SESSION
+    /* This is the code for the session increment button. It checks if the session time is greater than
+    0. If it is, it sets the countS variable to the session time, increments it by 1, and sets the
+    session time to the new countS variable. */
     $("#sessInc").on("click", function () {
         if ($("#session").html() > 0) {
             countS = parseInt($("#session").html());
@@ -36,6 +43,9 @@ $(document).ready(function () {
             //clock.setTime(countS*60);
         }
     });
+   /* This is the code for the session decrement button. It checks if the session time is greater than
+   1. If it is, it sets the countS variable to the session time, decrements it by 1, and sets the
+   session time to the new countS variable. */
     $("#sessDec").on("click", function () {
         if ($("#session").html() > 1) {
             countS = parseInt($("#session").html());
@@ -45,6 +55,11 @@ $(document).ready(function () {
         }
     });
     //BREAK
+    /* This is the code for the break increment and decrement buttons. It checks if the break time is
+    greater than
+    0 or 1, respectively. If it is, it sets the countB variable to the break time, increments or
+    decrements it by 1, and sets the
+    break time to the new countB variable. */
     $("#breakInc").on("click", function () {
         if ($("#break").html() > 0) {
             countB = parseInt($("#break").html());
@@ -59,6 +74,12 @@ $(document).ready(function () {
             $("#break").html(countB);
         }
     });
+    /* This is the code for the start button. It checks if the count variable is not equal to the
+    countS variable or if the clock is at 0. If it is, it sets the clock to the session time and
+    sets the
+    position to session. If it is not, it sets the position to the positionLama variable. It then
+    sets
+    the count variable to the countS variable and starts the clock. */
     $("#start").on("click", function () {
         if (count != countS || clock.getTime() == 0) {
             clock.setTime(countS * 60);
@@ -69,6 +90,9 @@ $(document).ready(function () {
         count = countS;
         clock.start();
     });
+    /* This is the code for the stop and clear buttons. The stop button stops the clock and sets the
+    countLama variable to the time on the clock. The clear button stops the clock, sets the position
+    to pomodoro, and sets the clock to 0. */
     $("#stop").on("click", function () {
         clock.stop();
         countLama = clock.getTime();
